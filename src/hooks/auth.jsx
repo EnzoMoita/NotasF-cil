b2cabc90f6e2fe0522a46364d20471a8eecd6bc1
Enzo Salomão <enzosadourado@hotmail.com>
2024-04-26 13:16:42 -0300

    authenticationadd

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,36 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { api } from '../services/api';
 
 const AuthContext = createContext({});
 
 function AuthProvider({ children }){
+    const [data, setData] = useState({});
+
+    
+
+
+    async function signIn({ email, password }){
+
+    try{
+
+            const response = await api.post("/sessions", { email, password });
+            const { user, token } = response.data;
+
+            api.defaults.headers.autorization = `Bearer ${token}`;
+            setData({ user, token })
+
+        }  catch (error) {
+            if(error.response){
+                alert(error.response.data.message);
+            }else{
+                alert("Não foi possivel logar!");
+
+            }
+        }
+    }
+
     return(
-        <AuthContext.Provider value={{ email: 'enzosadourado@hotmail.com' }}>
+        <AuthContext.Provider value={{ signIn, user: data.user }}>
             { children }
         </AuthContext.Provider>
     )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,5 @@
 import { Container, Form, Background } from './styles';
+import { useState } from 'react';
 import { useAuth } from '../../hooks/auth';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -6,9 +7,15 @@ import { FiMail, FiLock} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 export function SignIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const data = useAuth();
-    console.log("Meu contexto =>", data);
+
+    const {signIn} = useAuth();
+
+    function handleSignIn(){
+        signIn({ email, password });
+    }
     return(
         <Container>
             <Form>
@@ -21,15 +28,17 @@ export function SignIn(){
                 placeholder="E-mail"
                 type="text"
                 icon={FiMail}
+                onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                 placeholder="Senha"
                 type="password"
                 icon={FiLock}
+                onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar" />
+                <Button title="Entrar" onClick={handleSignIn} />
 
                 <Link to="/register" >
                     Criar conta
