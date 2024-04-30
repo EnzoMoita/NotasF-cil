21da6b97efe81888c61452664cb2bab657af1b5a
Enzo Salomão <enzosadourado@hotmail.com>
2024-04-29 22:32:56 -0300

    UpdateFilterTag

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,8 +9,10 @@ import { useState, useEffect } from 'react';
 import { api } from '../../services/api';
 
 export function Home(){
+    const [search, setSearch] = useState("");
     const [tags, setTags] = useState([]);
     const [tagsSelected, setTagsSelected] = useState([]);
+    const [notes, setNotes] = useState([]);
 
     function handleTagSelected(tagName){
         const alreadySelected = tagsSelected.includes(tagName);
@@ -37,6 +39,17 @@ export function Home(){
         fetchTags();
 
     },[]);
+
+
+    useEffect(() =>{
+        async function fetchNotes(){
+            const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);
+            setNotes(response.data);
+        }
+
+        fetchNotes();
+
+    },[tagsSelected, search]);
 
 
     return(
@@ -76,23 +89,26 @@ export function Home(){
             </Menu>
 
             <Search>
-                <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+                <Input 
+                placeholder="Pesquisar pelo título" icon={FiSearch}
+                onChange={() =>setSearch(e.target.value)}
+                
+                />
 
             </Search>
 
             <Content>
 
                 <Section title="Minhas notas">
-                    <Note data={{
-                        title: 'React', 
-                        tags: [
-                            {id: '1', name: 'react'},
-                            {id: '2', name: 'NotasFácil'},
+                   { 
+                    notes.map(note =>(
 
-                        
-                        ] 
-                        
-                        }} />
+                        <Note 
+                        key={String(note.id)}
+                        data={note} 
+                        /> 
+                    ))
+                    }
                         
 
                 </Section>
